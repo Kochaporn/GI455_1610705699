089893e415903241cf48ead0aa6bbfbef67654f2
DESKTOP-3D8TSTS\hanse <kochaporn.chan@bumail.net>
2021-01-31 13:07:01 +0700

    To Laptop

---
+++ GI455/Assets/ProgramChat/Scripts/WebsocketConnection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+namespace ProgramChat
+{
+    public class WebsocketConnection : MonoBehaviour
+    {
+        private WebSocket websocket;
+      
+        void Start()
+        {
+            websocket = new WebSocket("ws://127.0.0.1:25500/");
+
+            websocket.OnMessage += OnMessage;
+
+            websocket.Connect();
+
+            //websocket.Send("I'm coming here.");
+        }
+
+       
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                if(websocket.ReadyState == WebSocketState.Open)
+                {
+                    websocket.Send("Random number : "+ Random.Range(0, 999999));
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if(websocket != null)
+            {
+                websocket.Close();
+            }
+        }
+
+        public void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            Debug.Log("Recieved msg : " + messageEventArgs.Data);
+        }
+    }
+}
+
+
---
+++ Node js/server.js
@@ -0,0 +1,42 @@
+var websocket = require('ws');
+
+var callbackInitServer = ()=>{
+    console.log("hanzserver is running.");
+}
+
+var wss = new websocket.Server({port:25500}, callbackInitServer);
+
+var wslist = [];
+
+wss.on("connection", (ws)=>{
+
+    console.log("client connected.");
+    wslist.push(ws);
+
+    ws.on("message", (data)=>
+    {
+        console.log("send from client :" + data);
+        Broadcast(data);
+    });
+
+    ws.on("close", ()=>{
+        console.log("client disconnected.");
+        wslist = ArrayRemove(wslist, ws);
+    });
+
+});
+
+function ArrayRemove(arr, value)
+{
+    return arr.fillter((element)=>{
+        return element != value;
+    })
+}
+
+function Broadcast(data)
+{
+    for(var i = 0; i < wslist.length; i++)
+    {
+        wslist[i].send(data);
+    }
+}
